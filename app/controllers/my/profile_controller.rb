1b83199c26c4dd232dd8f7ad3a6ca6a320a0370a
query-string <atimofeev@reactant.ru>
2019-11-18 17:06:24 +0700

    Show words proficiency in profile

--- app/controllers/my/profile_controller.rb
+++ app/controllers/my/profile_controller.rb
@@ -2,5 +2,10 @@ class My::ProfileController < ApplicationController
   def index
     @person   = current_person
     @attempts = @person.attempts
+
+    proficiency ||= @person.proficiencies
+    @positive = proficiency.positive.by_attempts
+    @negative = proficiency.negative.by_attempts
+    @neutral  = proficiency.neutral.by_attempts
   end
 end
