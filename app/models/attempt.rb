142762a0e64b9223386fe22de5dd6767516a520e
query-string <atimofeev@reactant.ru>
2019-11-18 17:11:19 +0700

    Update Proficiency on each attempt

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -5,4 +5,10 @@ class Attempt < ApplicationRecord
   scope :date_groupped, -> { group_by { |a| a.created_at.beginning_of_day }.reverse_each }
   scope :success,       -> { where(status: :success) }
   scope :fail,          -> { where(status: :fail) }
+
+  after_save :update_proficiency
+
+  def update_proficiency
+    word.update_proficiency_of(person)
+  end
 end
