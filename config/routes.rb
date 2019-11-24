55cdf7c428963f1d551aba6aed23d08dcae32d9c
query-string <atimofeev@reactant.ru>
2019-11-24 23:02:15 +0700

    Add children attempts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :lists, only: %i[index show update]
+  resources :lists, only: %i[index show update] do
+    resources :attempts
+  end
   namespace :my do
     resources :words, only: %i[index show]
   end
