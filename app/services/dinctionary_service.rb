65fda752cb926e3717e31078bc752d59941390b7
query-string <atimofeev@reactant.ru>
2019-11-29 11:40:54 +0700

    Make Dictionary API more fall tolerant

--- app/services/dinctionary_service.rb
+++ app/services/dinctionary_service.rb
@@ -1,8 +1,14 @@
 class DinctionaryService
   def self.call(word)
-    url      = "https://www.dictionaryapi.com/api/v3/references/sd4/json/#{word}?key=#{ENV['DICTIONARY_API_KEY']}"
-    response = HTTParty.get(url)
+    url = "https://www.dictionaryapi.com/api/v3/references/sd4/json/#{word}?key=#{ENV['DICTIONARY_API_KEY']}"
 
-    response.success? ? response.parsed_response[0]["shortdef"] : []
+    begin
+      response = HTTParty.get(url)
+      response.success? ? response.parsed_response[0]["shortdef"] : []
+    rescue HTTParty::Error
+      []
+    rescue StandardError
+      []
+    end
   end
 end
